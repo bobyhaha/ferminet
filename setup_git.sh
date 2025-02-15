de0da2c82bbec7bbaf4ae5f5851518dad3ff7e66
baiyuzhu <baiyuzhu@mit.edu>
2025-02-15 02:14:27 +0000

    Update script & test git

--- setup_git.sh
+++ setup_git.sh
@@ -1,6 +1,7 @@
 git config --global user.name "baiyuzhu"
 git config --global user.email "baiyuzhu@mit.edu"
-mkdir -p ~/.ssh
-sudo chmod 700 ~/.ssh
-cp id_rsa ~/.ssh/
-sudo chmod 600 ~/.ssh/id_rsa
+
+mkdir -p /root/.ssh
+sudo chmod 700 /root/.ssh
+cp id_rsa /root/.ssh/
+sudo chmod 600 /root/.ssh/id_rsa
